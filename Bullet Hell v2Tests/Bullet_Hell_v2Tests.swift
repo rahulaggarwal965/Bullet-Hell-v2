4d78a2338090e4de7e1373f4f90ed19bcba9d032
Student <Student@dhcp-x235-137.mobile.uci.edu>
2016-07-06 11:37:17 -0700

    Initial Commit

---
+++ Bullet Hell v2Tests/Bullet_Hell_v2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Bullet_Hell_v2Tests.swift
+//  Bullet Hell v2Tests
+//
+//  Created by Student on 7/6/16.
+//  Copyright © 2016 DMA. All rights reserved.
+//
+
+import XCTest
+@testable import Bullet_Hell_v2
+
+class Bullet_Hell_v2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
